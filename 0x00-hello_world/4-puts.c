eeef4a68f4a3c7b42878da3f4af29269b4ad9277
Reallynoobcoder <elimal.hamza@gmail.com>
2023-06-15 12:17:14 -0700

    print with puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+/**
+ *  main is an entry point
+ *
+ *  return is the return value of the program
+ */
+int main(void)
+{
+	puts("Programming is like building a multilingual puzzle");
+	return (0);
+}
