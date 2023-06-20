960996ac0626b1f98cb102d47b4d58834b0c8209
Reallynoobcoder <elimal.hamza@gmail.com>
2023-06-20 11:16:22 -0700

    7-last

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -8,12 +8,11 @@ int print_last_digit(int i)
 {
 	int last;
 
-	if (i < 0)
-	{
-		i = i * -1;
-	}
 	last = i % 10;
-
+	if (last < 0)
+	{
+		last = last * -1;
+	}
 	_putchar(last + '0');
 
 	return (last);
