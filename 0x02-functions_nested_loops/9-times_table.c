ead938d8916aefe7cae47bdcb05f7497b4e3da0d
Reallynoobcoder <elimal.hamza@gmail.com>
2023-06-20 13:14:58 -0700

    .

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -9,21 +9,19 @@ void times_table(void)
 	for (n = 0; n <= 9; n++)
 	{
 		_putchar('0');
-	}
-	for (m = 1; m <= 9; m++)
-	{
-		_putchar(',');
-		_putchar(' ');
-		p = n * m;
-		if (p < 10)
+		for (m = 1; m <= 9; m++)
 		{
+			_putchar(',');
 			_putchar(' ');
+			p = n * m;
+			if (p < 10)
+				_putchar(' ');
+			else
+				_putchar((p / 10) + '0');
+
+			_putchar((p % 10) + '0');
 		}
-		else
-		{
-			_putchar((p / 10) + '0');
-		}
-		_putchar((p % 10) + '0');
+		_putchar('\n');
 	}
-	_putchar('\n');
 }
+
