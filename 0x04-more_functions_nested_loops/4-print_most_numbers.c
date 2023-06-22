fec839a0c0daecb05910844cd9be0875cd7dc348
Reallynoobcoder <elimal.hamza@gmail.com>
2023-06-22 04:26:16 -0700

    task 4

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
-* print_numbers - Prints numbers from 0 to 9 excluding 2 and 4
+* print_most_numbers - Prints numbers from 0 to 9 excluding 2 and 4
 */
 
 void print_most_numbers(void)
