b4c3a22db7ada7750a04092103d92bf478012bb7
Reallynoobcoder <elimal.hamza@gmail.com>
2023-06-26 07:02:55 -0700

    .

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+* _strlen - Calculates the length of a string.
+* @s: Pointer to the input string.
+*
+* Return: The length of the string.
+*/
+
+int _strlen(char *s)
+{
+	int l;
+
+	for (l = 0; s[l] != '\0'; l++)
+	{
+	}
+	return (l);
+}
