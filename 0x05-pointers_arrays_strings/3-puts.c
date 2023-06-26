a1f72d13c0665de4e992307c0a6f64344149bdad
Reallynoobcoder <elimal.hamza@gmail.com>
2023-06-26 07:14:54 -0700

    task 3

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+* _puts - Prints a string to the standard output.
+* @str: Pointer to the string to be printed.
+*
+*/
+
+void _puts(char *str)
+{
+	int i;
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		_putchar(str[i]);
+	}
+}
