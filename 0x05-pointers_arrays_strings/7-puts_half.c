011cabff58112ffcb5b85064101ed4f9bdd15066
Reallynoobcoder <elimal.hamza@gmail.com>
2023-06-26 11:01:44 -0700

    task 7

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,27 @@
+#include "main.h"
+/**
+* puts_half - Prints out the first half of a string.
+* @str: input string to print.
+*/
+
+void puts_half(char *str)
+{
+	int i, l, j;
+
+	for (l = 0; str[l] != '\0'; l++)
+	{
+	}
+	if (l % 2 == 0)
+	{
+		j = l / 2;
+	}
+	else
+	{
+		j = (l + 1) / 2;
+	}
+	for (i = start; str[i] != '\0'; i++)
+	{
+		_putchar(str[i]);
+	}
+	_putchar('\n');
+}
