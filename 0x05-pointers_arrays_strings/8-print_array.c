b4ec40c2678c361e3dd9afde6d7cbbfbd65d9dbc
Reallynoobcoder <elimal.hamza@gmail.com>
2023-06-26 11:32:41 -0700

    .

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -4,17 +4,18 @@
 * @a: Pointer to the integer array.
 * @n: Number of elements in the array.
 */
+
 void print_array(int *a, int n)
 {
 	int i;
 
-	for (i = 0; i < n; i++)
+	for (i = 0; a[i] != a[n]; i++)
 	{
 		printf("%d", a[i]);
-		if (i != n - 1)
+		if (a[i] != a[n - 1])
 		{
-			printf(", ");
+			printf(", ", a[i]);
 		}
 	}
-	printf("\n");
+	_putchar('\n');
 }
