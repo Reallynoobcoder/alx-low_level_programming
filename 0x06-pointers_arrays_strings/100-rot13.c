de488fc6443b8462e6157f9368b2084bc8833d15
Reallynoobcoder <elimal.hamza@gmail.com>
2023-06-30 12:50:02 -0700

    task 100

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * rot13 - encodes a string into rot13
+ * @str: string to encode
+ *
+ * Return: address of str
+ */
+char *rot13(char *str)
+{
+	int i, j;
+	char a[] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+	char b[] = "nopqrstuvwxyzabcdefghijklmNOPQRSTUVWXYZABCDEFGHIJKLM";
+
+	for (i = 0; str[i] != 0; i++)
+		for (j = 0; j < 52; j++)
+		{
+			if (str[i] == a[j])
+			{
+				str[i] = b[j];
+				break;
+			}
+		}
+	return (str);
+}
