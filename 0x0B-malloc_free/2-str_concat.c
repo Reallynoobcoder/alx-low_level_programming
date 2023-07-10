048090b0c89a3f467de8d55ed89f43afc2e62e4b
Reallynoobcoder <elimal.hamza@gmail.com>
2023-07-10 08:02:29 -0700

    task 2

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -1,24 +1,22 @@
 #include "main.h"
 #include <stdio.h>
 #include <stdlib.h>
-
 /**
-*_strncat - Concatenates two strings up to a specified
-*@dest: Pointer to the destination of the string.
+*_strcat - Concatenates two strings.
+*@dest: Pointer to the destination string.
 *@src: Pointer to the source string to be concatenated.
-*@n: Maximum number of characters from src to be concatenated.
 *Return: Pointer to the destination string after concatenation.
 */
 
-char *_strncat(char *dest, char *src, int n)
+char *_strcat(char *dest, char *src)
 {
 	int i, j;
 
 	for (i = 0; dest[i] != 0; i++)
 		;
-	for (j = 0; src[j] != 0 && j < n; i++, j++)
+	for (j = 0; src[j] != '\0'; j++)
 	{
-		dest[i] = src[j];
+		dest[i++] = src[j];
 	}
 	dest[i] = '\0';
 	return (dest);
