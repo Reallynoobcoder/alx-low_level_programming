cfd1c1fc80ae20d5aaaeb48d80db1aaa5211752d
Reallynoobcoder <elimal.hamza@gmail.com>
2023-07-13 04:31:10 -0700

    task 1

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -6,25 +6,33 @@
  * @s2: string to truncate up to n bytes
  * @n: number of bytes to truncate by
  * Return: pointer to new buffer
-*/
-
+ */
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-	char *ptr;
+	char *dest;
 	unsigned int len;
 
 	if (s1 == NULL)
-		s1 = ("");
+		s1 = "";
 	if (s2 == NULL)
-		s2 = ("");
+		s2 = "";
 
-	len = strlen(s1) + (strlen(s2) > n ? n : strlen(s2)) + 1;
-	ptr = malloc(sizeof(char) * len);
+	if (strlen(s2) <= n)
+		len = strlen(s1) + strlen(s2)  + 1;
+	else
+		len = strlen(s1) + n + 1;
 
-	strncpy(ptr, s1, len);
-	strncat(ptr, s2, n);
+	dest = malloc(sizeof(char) * len);
 
-	ptr[len - 1] = '\0';
+	if (dest == NULL)
+		return (NULL);
 
-	return (ptr);
+	strcpy(dest, s1);
+	if (n >= strlen(s2))
+		dest = strcat(dest, s2);
+	else
+		dest = strncat(dest, s2, n);
+	dest[len - 1] = '\0';
+
+	return (dest);
 }
