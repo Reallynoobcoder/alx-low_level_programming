fa6041ea5e7ec0b7eaab2c99c60ac53bac918d78
Reallynoobcoder <elimal.hamza@gmail.com>
2023-07-13 04:29:42 -0700

    .

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -14,9 +14,9 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	unsigned int len;
 
 	if (s1 == NULL)
-		return ("");
+		s1 = ("");
 	if (s2 == NULL)
-		return ("");
+		s2 = ("");
 
 	len = strlen(s1) + (strlen(s2) > n ? n : strlen(s2)) + 1;
 	ptr = malloc(sizeof(char) * len);
