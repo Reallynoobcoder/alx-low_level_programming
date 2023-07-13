4f07b81616bc7ecef3fee968f54d4ebf3eafa15b
Reallynoobcoder <elimal.hamza@gmail.com>
2023-07-13 05:18:31 -0700

    task 2

---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,24 @@
+#include <main.h>
+/**
+ * _calloc - allocates memory for an initialized buffer
+ * @nmemb: number of memory bytes
+ * @size: number of data type bits
+ * Return: return pointer to initialized buffer
+ */
+
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	int *ptr;
+
+	if(nmemb == 0 || size == 0)
+		return (NULL);
+
+	ptr = malloc (nmemb * size);
+
+	if(ptr == NULL)
+		return (NULL);
+
+	memset(ptr, 0, nmemb * size);
+
+	return (ptr);
+}
