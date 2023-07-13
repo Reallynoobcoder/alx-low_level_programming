7606cdfbaeecf835abf59cf09a77259d4fab3964
Reallynoobcoder <elimal.hamza@gmail.com>
2023-07-13 12:13:56 -0700

    .

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -1,16 +1,20 @@
 #include "main.h"
-#include <string.h>
 /**
-  * _realloc - reallocates a memory block
-  * @ptr: pointer to the original memory block
-  * @old_size: old size of memory block
-  * @new_size: new size of memory block
-  * Return: pointer to reallocated memory block
+ * _realloc - reallocates a memory block
+ * @ptr: pointer to the original memory block
+ * @old_size: old size of memory block
+ * @new_size: new size of memory block
+ * Return: pointer to reallocated memory block
  */
-
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 {
 	void *dest;
+
+	if (new_size == old_size)
+		return (ptr);
+
+	if (ptr == NULL)
+		return (malloc(new_size));
 
 	if (new_size == 0 && ptr != NULL)
 	{
@@ -18,27 +22,16 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 		return (NULL);
 	}
 
-	if (new_size == old_size)
-		return (ptr);
-
-	if (ptr == NULL)
-	{
-		return (malloc(new_size));
-	}
-
-	dest = malloc(new_size * sizeof(int));
+	dest = malloc(new_size);
 
 	if (dest == NULL)
 		return (NULL);
 
-	if (old_size < new_size)
-	{
+	if (new_size > old_size)
 		memcpy(dest, ptr, old_size);
-	}
+
 	else
-	{
 		memcpy(dest, ptr, new_size);
-	}
 
 	free(ptr);
 
