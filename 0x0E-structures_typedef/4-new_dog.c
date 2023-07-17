15dded2fa6f6eae4ff671f3e51ddc1e3c5960cec
Reallynoobcoder <elimal.hamza@gmail.com>
2023-07-17 11:56:59 -0700

    task 4

---
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -0,0 +1,31 @@
+#include "dog.h"
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+/**
+  * new_dog - creates a new struct of type dog
+  * @name: struct parameter name
+  * @age: struct parameter age
+  * @owner: struct parameter owner
+  * Return: returns pointer to buffer of datatype dog
+ */
+
+dog_t *new_dog(char *name, float age, char *owner)
+{
+	dog_t *newdog = malloc(sizeof(dog_t));
+
+	if (newdog == NULL)
+		return (NULL);
+
+	newdog->name = malloc(strlen(name) + 1);
+	newdog->owner = malloc(strlen(owner) + 1);
+
+	if (newdog->name == NULL || newdog->owner == NULL)
+		return (NULL);
+
+	strcpy(newdog->name, name);
+	strcpy(newdog->owner, owner);
+	newdog->age = age;
+
+	return (newdog);
+}
