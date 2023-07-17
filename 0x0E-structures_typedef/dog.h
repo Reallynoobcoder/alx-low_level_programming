dca8970c2ef0bd597a0d12d3ef790e98d289ec95
Reallynoobcoder <elimal.hamza@gmail.com>
2023-07-17 07:49:50 -0700

    task 0

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,12 +1,18 @@
 #ifndef DOG_H
 #define DOG_H
-
-/* Structure definition for a dog */
+/**
+ * struct dog - dog data
+ * @name: name of the dog
+ * @age: age of the dog
+ * @owner: name of the owner of the dog
+ *
+ * Description: Defines metadata about the dog and type of dog's struct
+*/
 
 struct dog
 {
 	char *name;
 	float age;
 	char *owner;
-};
+} dog;
 #endif
