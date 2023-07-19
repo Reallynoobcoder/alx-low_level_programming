404593cc4ad31f649a6dd947ca6a5526787e1df5
Reallynoobcoder <elimal.hamza@gmail.com>
2023-07-19 06:25:12 -0700

    task 2

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -7,12 +7,12 @@
   * Return: index of the first element found
  */
 
-int int_index(int *array, int size, int (*cmp)(int));
+int int_index(int *array, int size, int (*cmp)(int))
 {
 	int i;
 
 	if (!array || !size || !cmp)
-		return;
+		return (-1);
 
 	if (size <= 0)
 		return (-1);
