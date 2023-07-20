168789b5ad266d2ef69ff783f0415e5cce357251
Reallynoobcoder <elimal.hamza@gmail.com>
2023-07-20 08:54:06 -0700

    task 1, fifth try

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -11,15 +11,12 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 
 	unsigned int i;
 
-	if (separator == NULL)
-		exit(-1);
-
 	va_start(arg, n);
 
 	for (i = 0; i < n; i++)
 	{
 		printf("%d", va_arg(arg, int));
-		if (i < n - 1)
+		if (separator != NULL && i < n - 1)
 			printf("%s", separator);
 	}
 	printf("\n");
