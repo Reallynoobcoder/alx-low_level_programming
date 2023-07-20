945ecef95b2e1374e55990f2cdf77a6716e18f8a
Reallynoobcoder <elimal.hamza@gmail.com>
2023-07-20 08:47:22 -0700

    task1, second try

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -1,4 +1,9 @@
 #include "variadic_functions.h"
+/**
+  * print_numbers - prints numbers
+  * @separator: character to use to delimit
+  * @n: number of elements to print
+ */
 
 void print_numbers(const char *separator, const unsigned int n, ...)
 {
@@ -7,14 +12,14 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 	unsigned int i;
 
 	if (separator == NULL)
-		exit -1;
+		exit(-1);
 
 	va_start(arg, n);
 
 	for (i = 0; i < n; i++)
 	{
 		printf("%d", va_arg(arg, int));
-		if(i < n - 1)
+		if (i < n - 1)
 			printf("%s", separator);
 	}
 	printf("\n");
