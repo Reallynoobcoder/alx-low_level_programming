852e482c30d519b3883054aa5346ddbc88e94042
Elimali hamza <elimal.hamza@gmail.com>
2023-07-31 04:05:44 -0700

    task 2

---
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+
+/**
+ * add_nodeint - add new nodes to the list
+ * @head: current place in the list
+ * @n: int to add to the list
+ * Return: pointer to current position in list
+ */
+
+listint_t *add_nodeint(listint_t **head, const int n)
+{
+	listint_t *newnode = malloc(sizeof(listint_t));
+
+	if (newnode == NULL)
+		return (NULL);
+
+	newnode->n = n;
+	newnode->next = *head;
+	*head = newnode;
+
+	return (*head);
+}
