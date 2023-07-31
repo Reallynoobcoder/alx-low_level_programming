f87b5d168c44d51c3f7e3b715f3e597143beb062
Elimali hamza <elimal.hamza@gmail.com>
2023-07-31 10:14:52 -0700

    taks 5

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -8,7 +8,7 @@
 
 void free_listint2(listint_t **head)
 {
-	listint_t *temp = *head;
+	listint_t *temp;
 
 	if (head == NULL)
 		return;
@@ -21,3 +21,4 @@ void free_listint2(listint_t **head)
 	}
 	*head = NULL;
 }
+
