54d5ba9f996a68cc6c58ffd9d4a81d7d92737a89
Elimali hamza <elimal.hamza@gmail.com>
2023-08-07 11:37:02 -0700

    task 2

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -21,8 +21,7 @@ int append_text_to_file(const char *filename, char *text_content)
 
 	if (text_content == NULL)
 	{
-		close(fl);
-		return (-1);
+		text_content = "";
 	}
 	wr = write(fl, text_content, strlen(text_content));
 
