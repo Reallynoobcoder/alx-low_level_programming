8834d79abaabc8144524b62d08cfb9241e92d070
Elimali hamza <elimal.hamza@gmail.com>
2023-08-07 11:29:55 -0700

    task 2

---
+++ 0x15-file_io/2-append_text_to_file.c
@@ -0,0 +1,36 @@
+#include "main.h"
+/**
+ * append_text_to_file - appends text to a file
+ * @filename: file to append to, if NULL, return -1
+ * @text_content: content to append, if NULL, do not append
+ * Return: 1 on success, -1 on failure
+ */
+
+int append_text_to_file(const char *filename, char *text_content)
+{
+	int fl;
+
+	if (filename == NULL)
+		return (-1);
+
+	fl = open(filename, O_WRONLY | O_APPEND);
+
+	if (fl == -1)
+		return (-1);
+
+	if (text_content == NULL)
+	{
+		close(fl);
+		return (-1);
+	}
+	ssize_t wr = write(fl, text_content, strlen(text_content));
+
+	if (wr == -1)
+	{
+		close(fl);
+		return (-1);
+	}
+
+	close(fl);
+	return (1);
+}
