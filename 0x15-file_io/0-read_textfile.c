b011218645f99f6b380ff4d8f5cb8a928491cd18
Elimali hamza <elimal.hamza@gmail.com>
2023-08-07 08:13:41 -0700

    task 0

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,48 @@
+#include "main.h"
+
+/**
+ * read_textfile - reads a text file and prints it to the POSIX standard out
+ * @filename: name of the file to read
+ * @letters: number of characters to print
+ * Return: 0 on success
+ **/
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	int fl;
+	char *buffer;
+
+	if (filename == NULL)
+		return (0);
+
+	fl = open(filename, O_RDONLY);
+
+	if (fl == -1)
+		return (0);
+
+	buffer = malloc(letters);
+
+	if (buffer == NULL)
+	{
+		close(fl);
+		return (0);
+	}
+	ssize_t ra = read(fl, buffer, letters);
+
+	if (ra == -1)
+	{
+		free(buffer);
+		close(fl);
+		return (0);
+	}
+	ssize_t wr = write(STDOUT_FILENO, buffer, ra);
+
+	if (wr == -1)
+	{
+		free(buffer);
+		close(fl);
+		return (0);
+	}
+	free(buffer);
+	close(fl);
+	return (ra);
+}
